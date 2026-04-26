7921303d6d0c53dea208f7d815d4ad19ebeb303c
Javaher <javahernourian@gmail.com>
2026-04-26 13:36:24 +0330

    feat: define Book model in library_app

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,10 @@
+# library_app/models.py
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    author = models.CharField(max_length=100, verbose_name="Author")
+    published_year = models.IntegerField(verbose_name="Publication Year")
+
+    def __str__(self):
+        return self.title
