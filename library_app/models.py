e30b4ec1c2673a274c6bcd6485ab7b87ee615cd1
Javaher <javahernourian@gmail.com>
2026-04-30 18:51:14 +0330

    fix: allow user to be blank to avoid migration error

--- library_app/models.py
+++ library_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books', verbose_name='User')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books', verbose_name='User', null=True, blank=True)
     title = models.CharField(max_length=200, verbose_name="Title")
     author = models.CharField(max_length=100, verbose_name="Author")
     published_year = models.PositiveIntegerField(verbose_name="Publication Year")
