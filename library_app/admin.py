d1e37c73f1c2af707da3451d0f3036f1f9082619
Javaher <javahernourian@gmail.com>
2026-05-05 23:44:16 +0330

    add NoteInline to Book admin

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Book, Note
 
+class NoteInline(admin.TabularInline):
+    model = Note
+    extra = 1
+    fields = ('user', 'text', 'created_at')
+    readonly_fields = ('created_at',)
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "published_year")
+    inlines = [NoteInline]
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
