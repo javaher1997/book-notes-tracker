f431828735ec2b6d82737d5ee70f54b529880b76
Javaher <javahernourian@gmail.com>
2026-05-05 22:07:25 +0330

    add Note class

--- library_app/models.py
+++ library_app/models.py
@@ -10,3 +10,13 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes', verbose_name='User', null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='notes', verbose_name='Book', null=True, blank=True)
+    text = models.TextField(verbose_name='Text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return f'Note for {self.book.title} by {self.user.username}'
+    
