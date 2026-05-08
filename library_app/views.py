71fc7d2053ed15c72209719477b53daa236e386f
Javaher <javahernourian@gmail.com>
2026-05-08 19:13:37 +0330

    fix render arguments

--- library_app/views.py
+++ library_app/views.py
@@ -4,4 +4,4 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     context = {'books':books}
-    return render(request, book_list.html, context)
+    return render(request, 'book_list.html', context)
