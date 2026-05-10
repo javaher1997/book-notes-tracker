cfb47054196ea5949b7e09a3c1105e517aa56af7
Javaher <javahernourian@gmail.com>
2026-05-10 22:23:48 +0330

    provide url to add books

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('books/add/', views.book_create, name='book_create'),
 ]
