40cb316a8862a8551f41f11eb17036606acc2c48
Javaher <javahernourian@gmail.com>
2026-05-10 21:25:17 +0330

    add book detail fcn to views in order to provide details for each book

--- library_app/views.py
+++ library_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
     context = {'books':books}
     return render(request, 'book_list.html', context)
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    context = {'book':book}
+    return render(request, 'book_detail.html', context)
